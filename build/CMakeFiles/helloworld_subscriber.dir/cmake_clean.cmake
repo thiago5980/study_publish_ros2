9dd1e8dcf852bb6e25393639399943257db94986
thiago5980 <st9051@hanyang.ac.kr>
2023-01-01 17:28:41 +0900

    initial

---
+++ build/CMakeFiles/helloworld_subscriber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloworld_subscriber.dir/src/helloworld_subscriber.cpp.o"
+  "helloworld_subscriber"
+  "helloworld_subscriber.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloworld_subscriber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
